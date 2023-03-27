ddb7d7777c22b6e41de1ff1b475803380e61047e
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-03-27 10:57:56 -0700

    added tailwind and daisyui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -13,8 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </>
-  )
+  );
 }
