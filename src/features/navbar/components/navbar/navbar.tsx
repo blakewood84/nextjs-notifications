3c49fb51b7fd411df4d5206a1d45971fa3dd39c5
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-03-28 11:18:45 -0700

    updates

--- src/features/navbar/components/navbar/navbar.tsx
+++ src/features/navbar/components/navbar/navbar.tsx
@@ -2,11 +2,11 @@ import { ProfileIcon } from "../profile_icon";
 
 export function Navbar() {
   return (
-    <div className="navbar bg-primary">
-      <div className="navbar-start">
+    <div className="navbar bg-black">
+      <div className="navbar-start ml-10">
         <a className="btn btn-ghost normal-case text-xl">DROP</a>
       </div>
-      <div className="navbar-end">
+      <div className="navbar-end mr-20">
         <ProfileIcon />
       </div>
     </div>
--- src/features/navbar/components/profile_icon/profile_icon.tsx
+++ src/features/navbar/components/profile_icon/profile_icon.tsx
@@ -6,23 +6,27 @@ export function ProfileIcon() {
   const { user, switchUser } = useUser();
 
   return (
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img src={user.image} />
-        </div>
-      </label>
-      <ul
-        tabIndex={0}
-        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-      >
-        <li>
-          <a className="justify-between" onClick={() => switchUser()}>
-            Switch Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src={user.image} />
+          </div>
+        </label>
+
+        <ul
+          tabIndex={0}
+          className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a className="justify-between" onClick={() => switchUser()}>
+              Switch Profile
+              <span className="badge">New</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <span style={{ marginLeft: "5px" }}>{user.name}</span>
+    </>
   );
 }
