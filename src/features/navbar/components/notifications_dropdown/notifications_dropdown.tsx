8622b608dcc4e16c2d0ac56f2493b69fd171a7a8
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-03-29 20:09:36 -0700

    updates

--- src/features/navbar/components/notifications_dropdown/notifications_dropdown.tsx
+++ src/features/navbar/components/notifications_dropdown/notifications_dropdown.tsx
@@ -4,7 +4,7 @@ import Loading from "@/components/loading/loading";
 import { API_KEY } from "@/config/constants";
 import { useUser } from "@/stores/user";
 import { connect } from "getstream";
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function NotificationsDropdown() {
   const { token, user } = useUser();
@@ -27,7 +27,7 @@ export function NotificationsDropdown() {
       .feed("notification", user.id, token ?? "")
       .get({ enrich: true });
 
-    let list = [];
+    let list: any = [];
 
     for (const event of response.results) {
       const result = { ...event } as any;
@@ -39,11 +39,14 @@ export function NotificationsDropdown() {
       }
     }
 
-    // setNotifications(list);
+    // await setTimeout(() => {
+    //   setNotifications(list);
+    // }, 1000);
+    setNotifications(list);
   };
 
   return (
-    <div className="dropdown-content bg-base-200 text-base-content rounded-b-box top-px max-h-96 h-[70vh] w-52 overflow-y-auto shadow-2xl mt-16">
+    <div className="dropdown-content bg-base-200 text-base-content rounded-b-box top-px max-h-96 h-[70vh] w-64 overflow-y-auto shadow-2xl mt-16">
       <div tabIndex={0}>
         {notifications === null ? (
           <div className="flex justify-center">
@@ -104,11 +107,27 @@ function FollowNotification({
   } = data;
 
   const isUser = object.id === userId;
-  const verbPrefix = verb.split(" ")[0];
+  const verbPrefix = verb.split(" ")[0].toLowerCase();
+
+  const bgColor = !isSeen ? "bg-black" : "";
 
   return (
-    <div className="flex">
-      {actorName} {verbPrefix} {objectName}
+    <div
+      className={`flex flex-row items-center cursor-pointer p-4 mb-2 hover:bg-blue-500/50`}
+    >
+      <div style={{ width: "90%" }} className="flex flex-row items-center">
+        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img src={actorImage} />
+          </div>
+        </label>
+        <span style={{ fontSize: 12 }} className="ml-2">
+          {actorName} {verbPrefix} {isUser ? "you" : objectName}
+        </span>
+      </div>
+      <div style={{ width: "10%" }}>
+        <div className="ml-2 w-4 h-4 bg-blue-400 rounded-full"></div>
+      </div>
     </div>
   );
 }
